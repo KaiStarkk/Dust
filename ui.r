4797fe5fe31fb9b44453e20e1f11a227cd4eeddd
Kieran Hannigan <overyonderstudios@gmail.com>
2016-05-14 01:08:35 +0800

    Changed results > metrics

--- ui.r
+++ ui.r
@@ -9,10 +9,10 @@ dashboardSidebar(
     menuItem("Overview", tabName="overview", icon=icon("circle-o-notch")),
     menuSubItem("Plant Map", tabName="plant-map", icon=icon("map-o")),
     menuItem(
-      "Results", icon=icon("pie-chart"), collapsible=
-      menuSubItem("Result 1", tabName="result-1", icon=icon("line-chart")),
-      menuSubItem("Result 2", tabName="result-2", icon=icon("area-chart")),
-      menuSubItem("Result 3", tabName="result-3", icon=icon("bar-chart"))
+      "Metrics", icon=icon("pie-chart"), collapsible=
+      menuSubItem("Metric 1", tabName="metric-1", icon=icon("line-chart")),
+      menuSubItem("Metric 2", tabName="metric-2", icon=icon("area-chart")),
+      menuSubItem("Metric 3", tabName="metric-3", icon=icon("bar-chart"))
     ),
     menuItem("Recommendations", tabName="recommendations", icon=icon("comments"))
   )
@@ -22,9 +22,9 @@ dashboardBody(
   tabItems(
     tabItem(tabName="overview", h2("Overview"), p("Lorem ipsum dolor sit amet.")),
     tabItem(tabName="plant-map",h2("Plant Map"), p("Lorem ipsum dolor sit amet."), leafletOutput("sensorMap")),
-    tabItem(tabName="result-1", h2("Result 1"), p("Lorem ipsum dolor sit amet.")),
-    tabItem(tabName="result-2", h2("Result 2"), p("Lorem ipsum dolor sit amet.")),
-    tabItem(tabName="result-3", h2("Result 3"), p("Lorem ipsum dolor sit amet.")),
+    tabItem(tabName="metric-1", h2("Metric 1"), p("Lorem ipsum dolor sit amet.")),
+    tabItem(tabName="metric-2", h2("Metric 2"), p("Lorem ipsum dolor sit amet.")),
+    tabItem(tabName="metric-3", h2("Metric 3"), p("Lorem ipsum dolor sit amet.")),
     tabItem(tabName="recommendations", h2("Recommendations"), p("Lorem ipsum dolor sit amet."))
   )
 )
