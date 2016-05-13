92382f8b6d45503eac89129dbe3cb1cf237c1765
Kieran Hannigan <overyonderstudios@gmail.com>
2016-05-14 01:04:03 +0800

    Fixed UI leaflet import, added tab contents, changed style.

--- ui.r
+++ ui.r
@@ -1,13 +1,16 @@
+library(shiny)
 library(shinydashboard)
-dashboardPage(
+library(leaflet)
+
+dashboardPage(skin="yellow",
 dashboardHeader(title = "Dust"),
 dashboardSidebar(
   sidebarMenu(
     menuItem("Overview", tabName="overview", icon=icon("circle-o-notch")),
-    menuSubItem("Location", tabName="location", icon=icon("map-o")),
+    menuSubItem("Plant Map", tabName="plant-map", icon=icon("map-o")),
     menuItem(
-      "Results", icon=icon("pie-chart"), tabName="results", collapsible=
-        menuSubItem("Result 1", tabName="result-1", icon=icon("line-chart")),
+      "Results", icon=icon("pie-chart"), collapsible=
+      menuSubItem("Result 1", tabName="result-1", icon=icon("line-chart")),
       menuSubItem("Result 2", tabName="result-2", icon=icon("area-chart")),
       menuSubItem("Result 3", tabName="result-3", icon=icon("bar-chart"))
     ),
@@ -15,8 +18,14 @@ dashboardSidebar(
   )
 ),
 dashboardBody(
+  tags$style(type = "text/css", "#sensorMap {height: calc(91vh - 80px) !important;}"),
   tabItems(
-    tabItem(tabName="location", h2("Plant Map"), leafletOutput("sensorMap"))
+    tabItem(tabName="overview", h2("Overview"), p("Lorem ipsum dolor sit amet.")),
+    tabItem(tabName="plant-map",h2("Plant Map"), p("Lorem ipsum dolor sit amet."), leafletOutput("sensorMap")),
+    tabItem(tabName="result-1", h2("Result 1"), p("Lorem ipsum dolor sit amet.")),
+    tabItem(tabName="result-2", h2("Result 2"), p("Lorem ipsum dolor sit amet.")),
+    tabItem(tabName="result-3", h2("Result 3"), p("Lorem ipsum dolor sit amet.")),
+    tabItem(tabName="recommendations", h2("Recommendations"), p("Lorem ipsum dolor sit amet."))
   )
 )
 )
