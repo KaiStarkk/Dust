ea9a98ab1a03de91440711de37bff6a8d8937c4b
Kieran Hannigan <overyonderstudios@gmail.com>
2016-05-13 23:55:29 +0800

    Added location display.

---
+++ server.r
@@ -0,0 +1,16 @@
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+
+server <- function(input, output) {
+  sensorLocs=read.csv("MAP_Sensor_Locations.csv")
+  output$sensorMap <- renderLeaflet({
+    require(leaflet)
+    
+    m <- leaflet()
+    m <- addTiles(m)
+    
+    m <- addMarkers(m, lng=sensorLocs$Longitude, lat=sensorLocs$Latitude, popup=sensorLocs$Sample.Point)
+    
+  })
+}
--- ui.r
+++ ui.r
@@ -1,7 +1,22 @@
 library(shinydashboard)
-
 dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+dashboardHeader(title = "Dust"),
+dashboardSidebar(
+  sidebarMenu(
+    menuItem("Overview", tabName="overview", icon=icon("circle-o-notch")),
+    menuSubItem("Location", tabName="location", icon=icon("map-o")),
+    menuItem(
+      "Results", icon=icon("pie-chart"), tabName="results", collapsible=
+        menuSubItem("Result 1", tabName="result-1", icon=icon("line-chart")),
+      menuSubItem("Result 2", tabName="result-2", icon=icon("area-chart")),
+      menuSubItem("Result 3", tabName="result-3", icon=icon("bar-chart"))
+    ),
+    menuItem("Recommendations", tabName="recommendations", icon=icon("comments"))
+  )
+),
+dashboardBody(
+  tabItems(
+    tabItem(tabName="location", h2("Plant Map"), leafletOutput("sensorMap"))
+  )
+)
 )
