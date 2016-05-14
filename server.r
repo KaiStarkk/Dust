928c6342313f4d141d43a758ae7cab6c65e3e205
Kieran Hannigan <overyonderstudios@gmail.com>
2016-05-14 15:15:20 +0800

    Added alerts and sensor status indicators.

--- server.r
+++ server.r
@@ -4,12 +4,23 @@ library(leaflet)
 library(leafletplugins)
 
 server <- function(input, output) {
-  sensorLocs=read.csv("data/MAP_Sensor_Locations.csv")
+  sensorLocs = read.csv("data/MAP_Sensor_Locations.csv")
+  names = sensorLocs$Sample.Point
+  values = sample(1:100, length(names), replace=T)
+  current = data.frame(names, values)
+  
+  alerts = length(which(current$values >= 70))
+  alertStatus = "danger"
+  if (alerts <= 5) {
+    alertStatus = "warning"
+  }
+  
   sensorIcon <- makeIcon(
     iconUrl = "resources/sensor-icon.png",
     iconWidth = 20, iconHeight = 20,
     iconAnchorX = 10, iconAnchorY = 10
   )
+  
   output$sensorMap <- renderLeaflet({
     require(leaflet)
     m <- leaflet()
@@ -19,5 +30,31 @@ server <- function(input, output) {
                     popup=sensorLocs$Sample.Point, 
                     clusterOptions=markerClusterOptions(showCoverageOnHover = FALSE, maxClusterRadius = 20)
                     )
+  })
+  
+  if (alerts > 0) {
+    output$alertMenu <- renderMenu({
+      dropdownMenu(type="notifications", badgeStatus = alertStatus,
+                   notificationItem(
+                     text = paste(alerts, "sensors detecting >= 70ug/m3", sep=" "),
+                     icon = icon("exclamation-triangle"),
+                     status = "warning"
+                   ))
+    })
+  }
+  
+  output$statusMenu <- renderMenu({
+    statuses <- apply(current,1,function(row){
+      rowValue = row[["values"]]
+      rowName = row[["names"]]
+      rowStatus = "green"
+      if (rowValue >= 70) {
+        rowStatus = "red"
+      } else if (rowValue >= 35) {
+        rowStatus = "yellow"
+      }
+      taskItem(value=rowValue, color=rowStatus, rowName)
+    })
+    dropdownMenu(type="tasks", badgeStatus=alertStatus, .list=statuses)
   })
 }
--- ui.r
+++ ui.r
@@ -3,34 +3,38 @@ library(shinydashboard)
 library(leaflet)
 
 dashboardPage(skin="yellow",
-dashboardHeader(title = "Operation: Clear Skies"),
-dashboardSidebar(
-  sidebarMenu(
-    menuItem("Overview", tabName="overview", icon=icon("circle-o-notch")),
-    menuSubItem("Plant Map", tabName="plant-map", icon=icon("map-o")),
-    menuItem(
-      "Metrics", icon=icon("pie-chart"), collapsible=
-      menuSubItem("Metric 1", tabName="metric-1", icon=icon("line-chart")),
-      menuSubItem("Metric 2", tabName="metric-2", icon=icon("area-chart")),
-      menuSubItem("Metric 3", tabName="metric-3", icon=icon("bar-chart"))
-    ),
-    menuItem("Recommendations", tabName="recommendations", icon=icon("comments"))
-  )
-),
-dashboardBody(
-  tags$head(
-    tags$script(src = "https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js"),
-    tags$link(href="https://fonts.googleapis.com/css?family=Slabo+27px", rel="stylesheet", type="text/css"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
-    tags$link(href = "https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css", rel="stylesheet")
+  dashboardHeader(
+    title = "Operation: Clear Skies",
+    dropdownMenuOutput("alertMenu"),
+    dropdownMenuOutput("statusMenu")
   ),
-  tabItems(
-    tabItem(tabName="overview", h2("Overview"), p("Lorem ipsum dolor sit amet.")),
-    tabItem(tabName="plant-map",h2("Plant Map"), p("Lorem ipsum dolor sit amet."), leafletOutput("sensorMap")),
-    tabItem(tabName="metric-1", h2("Metric 1"), p("Lorem ipsum dolor sit amet.")),
-    tabItem(tabName="metric-2", h2("Metric 2"), p("Lorem ipsum dolor sit amet.")),
-    tabItem(tabName="metric-3", h2("Metric 3"), p("Lorem ipsum dolor sit amet.")),
-    tabItem(tabName="recommendations", h2("Recommendations"), p("Lorem ipsum dolor sit amet."))
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Overview", tabName="overview", icon=icon("circle-o-notch")),
+      menuSubItem("Plant Map", tabName="plant-map", icon=icon("map-o")),
+      menuItem(
+        "Metrics", icon=icon("pie-chart"), collapsible=
+        menuSubItem("Metric 1", tabName="metric-1", icon=icon("line-chart")),
+        menuSubItem("Metric 2", tabName="metric-2", icon=icon("area-chart")),
+        menuSubItem("Metric 3", tabName="metric-3", icon=icon("bar-chart"))
+      ),
+      menuItem("Recommendations", tabName="recommendations", icon=icon("comments"))
+    )
+  ),
+  dashboardBody(
+    tags$head(
+      tags$script(src = "https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js"),
+      tags$link(href="https://fonts.googleapis.com/css?family=Slabo+27px", rel="stylesheet", type="text/css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
+      tags$link(href = "https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css", rel="stylesheet")
+    ),
+    tabItems(
+      tabItem(tabName="overview", h2("Overview"), p("Lorem ipsum dolor sit amet.")),
+      tabItem(tabName="plant-map",h2("Plant Map"), p("Lorem ipsum dolor sit amet."), leafletOutput("sensorMap")),
+      tabItem(tabName="metric-1", h2("Metric 1"), p("Lorem ipsum dolor sit amet.")),
+      tabItem(tabName="metric-2", h2("Metric 2"), p("Lorem ipsum dolor sit amet.")),
+      tabItem(tabName="metric-3", h2("Metric 3"), p("Lorem ipsum dolor sit amet.")),
+      tabItem(tabName="recommendations", h2("Recommendations"), p("Lorem ipsum dolor sit amet."))
+    )
   )
-)
 )
